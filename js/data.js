e7d66dcae4e31014096015624b645ed37244e86b
gree-gorey <ignatyeff.g@yandex.ru>
2016-06-07 01:00:13 +0300

    create js

---
+++ js/data.js
@@ -0,0 +1,17 @@
+data = [
+    {
+        "source": "Madrid",
+        "destination": "Barcelona",
+        "type": "train"
+    },
+    {
+        "source": "Barcelona",
+        "destination": "Gerona Airport",
+        "type": "the airport bus"
+    },
+    {
+        "source": "Gerona Airport",
+        "destination": "Stockholm",
+        "type": "flight"
+    }
+];
